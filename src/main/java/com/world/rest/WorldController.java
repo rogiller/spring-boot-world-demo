ed6f69792c1ecb2b8399cc01ce8ab1e28d7881d4
Roger Diller <rogerdiller@gmail.com>
2016-01-23 16:48:47 -0500

    made into web app w/ a simple controller and greeting

---
+++ src/main/java/com/world/rest/WorldController.java
@@ -0,0 +1,25 @@
+package com.world.rest;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.world.rest.dto.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Roger Diller on 1/23/16.
+ */
+
+@RestController
+public class WorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ src/main/java/com/world/rest/dto/Greeting.java
@@ -0,0 +1,24 @@
+package com.world.rest.dto;
+
+/**
+ * Created by Roger Diller on 1/23/16.
+ */
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
